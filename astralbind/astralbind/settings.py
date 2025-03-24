28f1e93ed6eb7a6f4dd53ed94729c2119b5b5148
igorKornya <152435887+igorKornya@users.noreply.github.com>
2025-03-24 12:33:07 +0300

    1.2.1 db update

--- astralbind/astralbind/settings.py
+++ astralbind/astralbind/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'astralbind.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -95,7 +95,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
