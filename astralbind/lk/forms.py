7451916871b7212b5713a481ad7131f563667a7d
igorKornya <8kornya.igor8@mail.ru>
2025-04-13 21:01:52 +0300

    index, login, reg, main_page, profile, profile_edit, logout is done

--- astralbind/lk/forms.py
+++ astralbind/lk/forms.py
@@ -3,12 +3,29 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import UserProfile, Hobby
 
+
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    username = forms.CharField(
+        label='Логин',
+        min_length=4,
+        max_length=20,
+        widget=forms.TextInput(attrs={
+            'pattern': '[A-Za-z0-9_]{4,20}',
+            'title': 'Логин должен содержать от 4 до 20 символов (латинские буквы, цифры и подчеркивание)'
+        })
+    )
+
+    password1 = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={
+            'pattern': '^(?=.*[A-Za-z])(?=.*\d).{8,}$',
+            'title': 'Пароль должен содержать минимум 8 символов, включая буквы и цифры'
+        })
+    )
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ('username', 'email', 'password1', 'password2')
 
 class UserProfileForm(forms.ModelForm):
     hobbies = forms.ModelMultipleChoiceField(queryset=Hobby.objects.all(), widget=forms.CheckboxSelectMultiple, required=False)
