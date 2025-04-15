2c246ed09931b0d59a5650b573d2915eedb66775
Egor Porofiev <2vubj32cz@gmail.com>
2025-04-15 12:17:49 +0300

    cities added

--- astralbind/lk/models.py
+++ astralbind/lk/models.py
@@ -10,6 +10,30 @@ class Hobby(models.Model):
 
 
 class UserProfile(models.Model):
+    CITIES = [
+        (1, 'Москва'),
+        (2, 'Санкт-Петербург'),
+        (3, 'Новосибирск'),
+        (4, 'Екатеринбург'),
+        (5, 'Казань'),
+        (6, 'Нижний Новгород'),
+        (7, 'Челябинск'),
+        (8, 'Самара'),
+        (9, 'Омск'),
+        (10, 'Ростов-на-Дону'),
+        (11, 'Уфа'),
+        (12, 'Красноярск'),
+        (13, 'Воронеж'),
+        (14, 'Пермь'),
+        (15, 'Волгоград'),
+        (16, 'Краснодар'),
+        (17, 'Саратов'),
+        (18, 'Тюмень'),
+        (19, 'Тольятти'),
+        (20, 'Ижевск'),
+        (21, 'Липец'),
+        (22, 'Северодвинск'),
+    ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     photo = models.ImageField(
         upload_to = 'user_photos/',
@@ -21,6 +45,7 @@ class UserProfile(models.Model):
     hobby_description = models.TextField(blank=True, null=True)
     life_goal = models.TextField(blank=True, null=True)
     character = models.TextField(blank=True, null=True)
+    city = models.PositiveSmallIntegerField(('city'), choices=CITIES, blank=True, default=1) #!временно пустые строки разрешены, сначала раздадим всем пользователям города
 
     def save(self, *args, **kwargs):
         if self.pk:  # Только для существующих объектов
